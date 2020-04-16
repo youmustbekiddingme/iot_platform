49014efedf21c0f94918b8494ff9c8557463f46c
st89203621 <908321811@qq.com>
2020-04-16 16:09:19 +0800

    manage.vue页面增加 分页。

--- iot_dmp_frontend/src/pages/manage/manage.vue
+++ iot_dmp_frontend/src/pages/manage/manage.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
-      <el-container style="height: 500px; border: 1px solid #eee">
+      <el-container style="height: 900px; border: 1px solid #eee">
         <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
           <el-menu :default-openeds="['1', '5']">
 
             <el-submenu index="1">
               <template slot="title"  >
-                <div id="menu1" @click="userClick">
+                <div id="menu1" @click="setIsShow('isShowUser')">
                   <img :src="userImgUrl" class="elImg" >  用户管理
                 </div>
               </template>
@@ -14,7 +14,7 @@
 
             <el-submenu index="2">
               <template slot="title">
-                <div id="menu2" @click="netClick">
+                <div id="menu2" @click="setIsShow('isShowNetwork')">
                   <img :src="wifiImgUrl" class="elImg" >   网络测试
                 </div>
               </template>
@@ -22,7 +22,7 @@
 
             <el-submenu index="3">
               <template slot="title">
-                <div id="menu3" @click="burnClick">
+                <div id="menu3" @click="setIsShow('isShowCom')">
                   <img :src="settingUrl" class="elImg" >   串口烧录
                 </div>
               </template>
@@ -30,7 +30,7 @@
 
             <el-submenu index="4">
               <template slot="title">
-                <div id="menu4" @click="deviceClick">
+                <div id="menu4" @click="setIsShow('isShowDevice')">
                   <img :src="deviceUrl" class="elImg" >  设备管理
                 </div>
               </template>
@@ -38,7 +38,7 @@
 
             <el-submenu index="5">
               <template slot="title">
-                <div id="menu5" @click="gisClick">
+                <div id="menu5" @click="setIsShow('isShowGis')">
                   <img :src="gisUrl" class="elImg" >  Gis管理
                 </div>
               </template>
@@ -48,16 +48,24 @@
         </el-aside>
 
         <el-container>
+
+
           <el-header >
-                高菲物联后台管理系统
+                搜索条件框
           </el-header>
 
 
           <el-main  v-show="isShowUser" >
             <el-table :data="tableData">
-              <el-table-column prop="date" label="日期" width="140">
+              <el-table-column prop="realname" label="真实姓名" width="120">
               </el-table-column>
-              <el-table-column prop="name" label="姓名" width="120">
+              <el-table-column prop="nickname" label="昵称" width="140">
+              </el-table-column>
+              <el-table-column prop="phone" label="手机号" width="140">
+              </el-table-column>
+              <el-table-column prop="email" label="邮箱" width="250">
+              </el-table-column>
+              <el-table-column prop="regTime" label="注册时间" width="140">
               </el-table-column>
               <el-table-column prop="address" label="地址">
               </el-table-column>
@@ -91,6 +99,19 @@
             Gis管理
           </el-main>
 
+          <!--footer.............-->
+          <el-footer>
+            <div class="block">
+              <el-pagination
+                @size-change="handleSizeChange"
+                @current-change="handleCurrentChange"
+                :current-page="currentPage4"
+                :page-sizes="[10, 20, 30, 40]"
+                layout="total, sizes, prev, pager, next, jumper"
+                :total="100">
+              </el-pagination>
+            </div>
+          </el-footer>
         </el-container>
 
       </el-container>
@@ -101,8 +122,11 @@
   export default {
     data() {
       const item = {
-        date: '2016-05-02',
-        name: '王小虎',
+        realname: '王小虎',
+        nickname:'laowang',
+        phone:'18320966643',
+        email:'545018520@qq.com',
+        regTime: '2016-05-02',
         address: '上海市普陀区金沙江路 1518 弄'
       };
       return {
@@ -111,8 +135,8 @@
         settingUrl:require("../../assets/imgs/设置.png"),
         deviceUrl:require("../../assets/imgs/设备.png"),
         gisUrl:require("../../assets/imgs/GIS应用.png"),
-        tableData: Array(20).fill(item),
-        isShowUser:false,
+        tableData: Array(10).fill(item),
+        isShowUser:true,
         isShowNetwork:false,
         isShowCom:false,
         isShowDevice:false,
@@ -120,41 +144,14 @@
       }
     },
     methods:{
-      userClick(){
-        this.isShowUser=true;
+      setIsShow(type){
+        this.isShowUser=false;
         this.isShowNetwork=false;
         this.isShowCom=false;
         this.isShowDevice=false;
         this.isShowGis=false;
-      },
-      netClick(){
-        this.isShowUser=false;
-        this.isShowNetwork=true;
-        this.isShowCom=false;
-        this.isShowDevice=false;
-      },
-      burnClick(){
-        this.isShowUser=false;
-        this.isShowNetwork=false;
-        this.isShowCom=true;
-        this.isShowDevice=false;
-        this.isShowGis=false;
-
-      },
-      deviceClick(){
-        this.isShowUser=false;
-        this.isShowNetwork=false;
-        this.isShowCom=false;
-        this.isShowDevice=true;
-        this.isShowGis=false;
-
-      },
-      gisClick(){
-        this.isShowUser=false;
-        this.isShowNetwork=false;
-        this.isShowCom=false;
-        this.isShowDevice=false;
-        this.isShowGis=true;
+        this[type]=true;
+        //do request search web
       }
 
     }
@@ -163,14 +160,22 @@
 
 <style scoped>
   .el-header {
-    background-color: #B3C0D1;
+    /*background-color: #B3C0D1;*/
     color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-footer {
+    margin:100px  5px  100px  5px;
+    color: #333;
+    text-align: center;
     line-height: 60px;
   }
 
   .el-aside {
     color: #333;
   }
+
   .elImg{
     width:15px;
     height:15px;
