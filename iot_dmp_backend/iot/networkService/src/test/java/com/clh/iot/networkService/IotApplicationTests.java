863d40a1ebb22ac4e88bf0245afd05226a51e1c3
st89203621 <908321811@qq.com>
2020-04-20 11:35:02 +0800

    单元测试

--- iot_dmp_backend/iot/networkService/src/test/java/com/clh/iot/networkService/IotApplicationTests.java
+++ iot_dmp_backend/iot/networkService/src/test/java/com/clh/iot/networkService/IotApplicationTests.java
@@ -1,16 +1,28 @@
-//package com.clh.iot.network;
-//
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-//@SpringBootTest
-//class IotApplicationTests {
-//
-//	@Test
-//	void contextLoads() {
-//		Long time =  System.currentTimeMillis();
-//		//   1585813982380
-//		System.out.println(time);
-//	}
-//
-//}
+package com.clh.iot.networkService;
+
+import com.clh.iot.networkService.dao.TUserMapper;
+import com.clh.iot.networkService.pojo.TUser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = IotApplication.class)
+public class IotApplicationTests {
+    @Autowired
+    private TUserMapper userMapper;
+
+	@Test
+	public void test(){
+	    for(int i=0;i<23;i++){
+            TUser tUser = new TUser();
+            tUser.setNote("hh");
+            tUser.setSex(1);
+            tUser.setUserName("caolihua"+i);
+            userMapper.insert(tUser);
+        }
+    }
+
+}
