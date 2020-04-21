eb2be09732110cf3a81281fac65acb50414de07a
st89203621 <908321811@qq.com>
2020-04-21 16:58:09 +0800

    逆向工程

---
+++ iot_dmp_backend/iot/generator/src/main/java/com/clh/iot/networkService/dao/UserMapper.java
@@ -0,0 +1,17 @@
+package com.clh.iot.networkService.dao;
+
+import com.clh.iot.networkService.pojo.User;
+
+public interface UserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
